11771c4ecba80d1c73fc5e6e23539650aab29a3e
Wonders <wondersprince1@gmail.com>
2023-07-18 09:59:57 -0700

    Function 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 /**
  * _isalpha - checks if a character is an alphabet
  * @c: character to check
- * Return - Returns 1 if alphabet and 0 if not
+ * Return: Returns 1 if alphabet and 0 if not
  */
 int _isalpha(int c)
 {
