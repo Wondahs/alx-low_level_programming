9e29abf075249d9b1bb193ca2ea34fd7b1eb3530
Wonders <wondersprince1@gmail.com>
2023-07-18 08:19:12 -0700

    Functions in C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_alphabet - prints lowercase alphabet
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - prints lowercase alphabets ten times
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			putchar(j);
+		}
+		putchar('\n');
+	}
+}
