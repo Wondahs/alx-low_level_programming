0b5188350b62f002f4385678f44262dba6f28658
Wonders <wondersprince1@gmail.com>
2023-07-18 19:07:12 -0700

    Finally

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,7 +16,11 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 			k = i * j;
-			if (j != 0)
+			if (j == 0)
+			{
+				printf("%d, ", k);
+			}
+			else if (j != 0)
 			{
 				printf("%2d", k);
 				if (j < n)
