d5b481dd48b8ebf87e74d285e0bf6faa801378d6
Wonders <wondersprince1@gmail.com>
2023-07-24 04:17:06 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - pointer to reset value to 98
+ *
+ *@n: input value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Learning about C pointers and strings
