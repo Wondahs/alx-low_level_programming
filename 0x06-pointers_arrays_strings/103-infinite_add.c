20958b6a99fe6616f3c27d5a8af3b5291d1405e4
Wonders <wondersprince1@gmail.com>
2023-07-27 09:24:04 -0700

    This must work

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * rev_string - Reverses an array (string).
  * @n: The array (string) to be reversed.
@@ -32,7 +31,6 @@ void rev_string(char *n)
  * @n2: Text representation of the second number to add.
  * @r: Pointer to the buffer to store the result.
  * @size_r: Buffer size.
- *
  * Return: Pointer to the calling function.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
@@ -46,24 +44,19 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		j++;
 	i--;
 	j--;
-
 	if (j >= size_r || i >= size_r)
 		return (0);
-
 	while (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
 			val1 = 0;
 		else
 			val1 = *(n1 + i) - '0';
-
 		if (j < 0)
 			val2 = 0;
 		else
 			val2 = *(n2 + j) - '0';
-
 		temp_tot = val1 + val2 + overflow;
-
 		if (temp_tot >= 10)
 			overflow = 1;
 		else
@@ -77,10 +70,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		j--;
 		i--;
 	}
-
 	if (digits == size_r)
 		return (0);
-
 	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
