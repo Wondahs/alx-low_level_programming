6ec699ec0a48863047095d83f317a42864e44d3d
Wonders <wondersprince1@gmail.com>
2023-07-31 06:19:42 -0700

    corrected description

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 /**
  *_memcpy - copies n bytes from memory area src to memory area dest
  *@dest: destination memory area
- *@scr: source memory area
+ *@src: source memory area
  *@n: number of bytes to copy
  *Return: copied destination memory area
  */
