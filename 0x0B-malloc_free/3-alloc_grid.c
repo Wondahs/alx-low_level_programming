5ec08780b0a6cdfe6a281bcecf3952f35ba2c0c9
Wonders <wondersprince1@gmail.com>
2023-08-07 07:59:09 -0700

    Malloc 3 Try 2

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	matrix = (int **)malloc(height * sizeof(int));
+	matrix = (int **)malloc(height * sizeof(int *));
 
 	for (i = 0; i < height; i++)
 	{
