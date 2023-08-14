246fafd19ce02b2998f429d90b262a06539bdd1c
Wonders Victor <wondersprince1@gmail.com>
2023-08-14 16:02:09 +0100

    Struct 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - Dog structure
+ *@name: Name of dog
+ *@age: Age of dog
+ *@owner: Owner of dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
