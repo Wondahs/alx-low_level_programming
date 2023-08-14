feab7dce563ee297da99f6d6beddc133a187b925
Wonders Victor <wondersprince1@gmail.com>
2023-08-14 14:32:44 +0100

    MACRO 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef FUNCTION_LIKE_MACRO_H_3
 #define FUNCTION_LIKE_MACRO_H_3
-#define ABS(x) ((x) < (0) ? (-x) : (x))
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 #endif /* FUNCTION_LIKE_MACRO_H_3 */
