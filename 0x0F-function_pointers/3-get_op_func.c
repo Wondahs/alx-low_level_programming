89856145813f05b28b46304616aaf490d7b523e6
Wonders Victor <wondersprince1@gmail.com>
2023-08-17 14:20:55 +0100

    Task 3 fix 2

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -14,14 +14,14 @@ int (*get_op_func(char *s))(int, int)
 	{"-", op_sub},
 	{"*", op_mul},
 	{"/", op_div},
-        {"%", op_mod},
+	{"%", op_mod},
 	{NULL, NULL}
 	};
-    int i;
+	int i;
 
-    i = 0;
+	i = 0;
 
-    while ((ops[i].op) != NULL && *(ops[i].op) != *s)
-	    i++;
-    return (ops[i].f);
+	while ((ops[i].op) != NULL && *(ops[i].op) != *s)
+		i++;
+	return (ops[i].f);
 }
