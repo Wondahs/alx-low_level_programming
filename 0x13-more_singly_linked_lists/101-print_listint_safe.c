d09c2a3d37d11b4fecfbd79936c553ff9fcda29b
Wonders Victor <wondersprince1@gmail.com>
2023-08-29 08:02:45 +0100

    101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,29 +3,68 @@
 #include <stdlib.h>
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely
- * @head: Pointer to the head of the list
+ * detect_loop - Detects a loop in a linked list
+ * @head: Pointer to the head of the linked list
  *
- * Return: Number of nodes in the list
+ * Return: The node where the loop starts, or NULL if no loop
+ */
+listint_t *detect_loop(listint_t *head)
+{
+	listint_t *tortoise = head;
+	listint_t *hare = head;
+
+	while (hare && hare->next)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		if (tortoise == hare)
+			return (tortoise); /* Loop detected, return the meeting point */
+	}
+
+	return (NULL); /* No loop detected */
+}
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current = head;
-	size_t count = 0;
+	listint_t *copy = NULL;
+	listint_t *loop_node;
+	size_t nodes = 0;
 
-	while (current != NULL)
+	if (head)
+		copy = (listint_t *)head;
+
+	loop_node = detect_loop(copy);
+
+	if (loop_node == NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
-
-		if (current <= current->next)
+		while (head != NULL)
 		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			exit(98);
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+			nodes++;
+		}
+	}
+	else
+	{
+		const listint_t *current = head;
+
+		while (current != loop_node)
+		{
+			printf("[%p] %d\n", (void *)current, current->n);
+			current = current->next;
+			nodes++;
 		}
 
-		current = current->next;
+		printf("-> [%p] %d\n", (void *)loop_node, loop_node->n);
+		nodes++;
 	}
 
-	return (count);
+	return (nodes);
 }
