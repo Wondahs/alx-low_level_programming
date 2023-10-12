30a03a4728c21d0b0478cdd32655a921e04680ed
Wonders Victor <wondersprince1@gmail.com>
2023-10-12 23:19:27 +0000

    Moew fixss doe 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0, length = 0;
 	dlistint_t *current, *temp, *newNode;
 
-	if (!n)
+	if (!n || !idx)
 		return (NULL);
 	current = *h;
 	length = check_length(current);
