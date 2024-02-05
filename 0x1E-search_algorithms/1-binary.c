63669fa5bf19a8a72da5580c2c0d836b83dbfc7b
Wonders Victor <wondersprince1@gmail.com>
2024-02-05 19:32:21 +0100

    Working

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,19 @@
+#include "search_algos.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int index = 0, mid;
+
+	if (array == NULL)
+		return (-1);
+	mid = int(size) / 2;
+	if (value)
+}
